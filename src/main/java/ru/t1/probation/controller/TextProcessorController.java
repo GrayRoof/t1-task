fb1c2035be7c2f64629521a8f2c3ecfde133f090
Grayroof <1grayroof@gmail.com>
2023-11-27 13:57:02 +0300

    feat: make controller to call service

--- src/main/java/ru/t1/probation/controller/TextProcessorController.java
+++ src/main/java/ru/t1/probation/controller/TextProcessorController.java
@@ -1,17 +1,24 @@
 package ru.t1.probation.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.t1.probation.service.TextProcessorService;
+
+import java.util.Map;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class TextProcessorController {
-    private static final Logger log = LoggerFactory.getLogger(TextProcessorController.class);
+
+    private final TextProcessorService textProcessorService;
 
     @GetMapping()
-    public String compute() {
-        log.info("compute results");
-        return "compute results";
+    public Map<Character, Integer> process(@RequestParam(defaultValue = "") String text) {
+        log.info("processing given text...");
+        return textProcessorService.getFrequencyOfChar("ppaaaayyrttttt");
     }
 }
